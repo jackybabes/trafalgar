f821dcd09e160478ace221c05986c50c57f7f5fc
Jack Tricker <jackcbtricker@gmail.com>
2020-07-25 12:42:45 +1000

    Updates

---
+++ precache-manifest.1e73e3a2c02bcede495b167e431bc19e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95596a89d741231450d83dcc123504ec",
+    "url": "/trafalgar/index.html"
+  },
+  {
+    "revision": "d2b7bf7936b0e0a7f863",
+    "url": "/trafalgar/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "459d4b93a01b875f8dfc",
+    "url": "/trafalgar/static/css/main.b542bcc1.chunk.css"
+  },
+  {
+    "revision": "d2b7bf7936b0e0a7f863",
+    "url": "/trafalgar/static/js/2.1676d2f5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/trafalgar/static/js/2.1676d2f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459d4b93a01b875f8dfc",
+    "url": "/trafalgar/static/js/main.b52043b1.chunk.js"
+  },
+  {
+    "revision": "adfaeb5dd366582f630c",
+    "url": "/trafalgar/static/js/runtime-main.4d4cc7ef.js"
+  },
+  {
+    "revision": "efffffa665484f05c60241712608756a",
+    "url": "/trafalgar/static/media/play2.efffffa6.svg"
+  }
+]);
